f9caa6f73638ff69e8f73f04eac6fce637189b60
unknown <csaxe@christoph.ixite.local>
2016-04-11 19:48:57 -0700

    cleans up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var Eyeglass = require('eyeglass');
 
-var eyeglass = new Eyeglass({
-  // ... node-sass options
-    // importer: function(uri, prev, done) {
-    //     done(sass.compiler.types.NULL);
-    // }
-});
-
-// Disable import once with gulp until we
-// figure out how to make them work together.
-// eyeglass.enableImportOnce = false
+var eyeglass = new Eyeglass();
 
 gulp.task('sass', function() {
-	console.log(eyeglass.options);
 	return gulp.src('src/core/*.scss')
 		.pipe(sass(eyeglass.options).on('error', sass.logError))
 		.pipe(gulp.dest('dist/css'));
